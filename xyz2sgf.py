7dbc860dc2dfb1fea882dd7da972db35bc2f669c
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-03-26 18:14:26 +0000

    handle handicaps on < 19 in NGF, fix handicap 3 on Tygem

--- xyz2sgf.py
+++ xyz2sgf.py
@@ -21,18 +21,6 @@ class UnknownFormat(Exception): pass
 
 EMPTY, BLACK, WHITE = 0, 1, 2
 
-handicap_points_19 = {
-    0: [],
-    1: [],
-    2: [(16,4), (4,16)],
-    3: [(16,4), (4,16), (16,16)],
-    4: [(16,4), (4,16), (16,16), (4,4)],
-    5: [(16,4), (4,16), (16,16), (4,4), (10,10)],
-    6: [(16,4), (4,16), (16,16), (4,4), (4,10), (16,10)],
-    7: [(16,4), (4,16), (16,16), (4,4), (4,10), (16,10), (10,10)],
-    8: [(16,4), (4,16), (16,16), (4,4), (4,10), (16,10), (10,4), (10,16)],
-    9: [(16,4), (4,16), (16,16), (4,4), (4,10), (16,10), (10,4), (10,16), (10,10)]
-}
 
 
 class Node():
@@ -84,6 +72,58 @@ def safe_string(s):     # "safe" meaning safely escaped \ and ] characters
             safe_s += "\\"
         safe_s += ch
     return safe_s
+
+
+def handicap_points(boardsize, handicap, tygem = False):
+
+    points = set()
+
+    if boardsize < 4:
+        return points
+
+    if handicap > 9:
+        handicap = 9
+
+    if boardsize < 13:
+        d = 2
+    else:
+        d = 3
+
+    if handicap >= 2:
+        points.add((boardsize - d, 1 + d))
+        points.add((1 + d, boardsize - d))
+
+    # Experiments suggest Tygem puts its 3rd handicap stone in the top left
+
+    if handicap >= 3:
+        if tygem:
+            points.add((1 + d, 1 + d))
+        else:
+            points.add((boardsize - d, boardsize - d))
+
+    if handicap >= 4:
+        if tygem:
+            points.add((boardsize - d, boardsize - d))
+        else:
+            points.add((1 + d, 1 + d))
+
+    if boardsize % 2 == 0:      # No handicap > 4 on even sided boards
+        return points
+
+    mid = (boardsize + 1) // 2
+
+    if handicap in [5, 7, 9]:
+        points.add((mid, mid))
+
+    if handicap in [6, 7, 8, 9]:
+        points.add((1 + d, mid))
+        points.add((boardsize - d, mid))
+
+    if handicap in [8, 9]:
+        points.add((mid, 1 + d))
+        points.add((mid, boardsize - d))
+
+    return points
 
 
 def load(filename):
@@ -163,9 +203,6 @@ def write_tree(outfile, node):      # Relies on values already being correctly b
 
 def parse_ugf(ugf):     # Note that the files are often (always?) named .ugi
 
-    # This format better documented than some, see:
-    # http://homepages.cwi.nl/~aeb/go/misc/ugf.html
-
     root = Node(parent = None)
     node = root
 
@@ -211,7 +248,7 @@ def parse_ugf(ugf):     # Note that the files are often (always?) named .ugi
                     handicap_str = line.split("=")[1].split(",")[0]
                     handicap = int(handicap_str)
                     if handicap >= 2:
-                        root.set_value("HA", handicap)
+                        root.set_value("HA", handicap)      # The actual stones are placed in the data section
 
                     komi_str = line.split("=")[1].split(",")[1]
                     komi = float(komi_str)
@@ -331,9 +368,6 @@ def parse_ngf(ngf):
     if boardsize < 1 or boardsize > 19 or handicap < 0 or handicap > 9:
         raise ParserFail
 
-    if boardsize != 19 and handicap >= 2:     # Can't be bothered
-        raise ParserFail
-
     root = Node(parent = None)
     node = root
 
@@ -343,7 +377,7 @@ def parse_ngf(ngf):
 
     if handicap >= 2:
         root.set_value("HA", handicap)
-        stones = handicap_points_19[handicap]
+        stones = handicap_points(boardsize, handicap)
         for point in stones:
             root.add_value("AB", string_from_point(point[0], point[1]))
 
@@ -399,10 +433,6 @@ def parse_ngf(ngf):
 
 def parse_gib(gib):
 
-    # .gib is a file format used by the Tygem server, it's undocumented.
-    # I know nothing about how it specifies board size or variations.
-    # I've inferred from other source code how it does handicaps.
-
     root = Node(parent = None)
     node = root
 
@@ -436,7 +466,7 @@ def parse_gib(gib):
 
             if handicap >= 2:
                 node.set_value("HA", handicap)
-                stones = handicap_points_19[handicap]
+                stones = handicap_points(19, handicap, tygem = True)
                 for point in stones:
                     node.add_value("AB", string_from_point(point[0], point[1]))
 
